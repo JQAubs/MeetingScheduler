2d8166faa872eabdf6743a2b25c45b67bc4cd2f2
JQAubs <89174571+JQAubs@users.noreply.github.com>
2021-11-06 17:40:28 -0500

    updated

--- schedule.py
+++ schedule.py
@@ -1,16 +1,30 @@
-import calendar
 import datetime
 import time
 import entry
+import person
 
 
 class schedule:
 
     def __init__(self, user):
-        self.calendar = calendar()
-        self.dateTime = datetime.today()
+        self.dateTime = datetime.date.today()
         self.person = user
-        
+        self.events = dict()
+
     def addEntry(self, new_entry):
-        
-        
+        if new_entry.start_time in self.events:
+            self.events[str(new_entry.start_time.year)+str(new_entry.start_time.month)+str(new_entry.start_time.day)].append(new_entry)
+        else:
+            self.events[str(new_entry.start_time.year)+str(new_entry.start_time.month)+str(new_entry.start_time.day)] = [new_entry]
+
+    def getEvents(self):
+        return self.events.copy()
+
+#ent1 = entry.Entry(datetime.datetime.today(), datetime.datetime.now(), 'test', 'fuck it')
+#s1 = schedule(None)
+#dt = datetime.datetime.today()
+#print(dt.day, dt.month)
+
+#s1.addEntry(ent1)
+#print(s1)
+#print(s1.events)
