7cef069f7a9626d46a41f951e4aaba84737052f4
Ziyang Li <60530557+Ziyangll@users.noreply.github.com>
2021-11-06 14:44:08 -0500

    demo

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+
+    def __init__(self):
+        self.name = ""
+        self.age = 0
+        self.schedule = Schedule()
+    
+    def addEntry(self):
+        return
+    def createGroup(self):
+        return
+    
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 def main():
+    # how the demo will go
+    print("start demo")
+    print("create person A")
+    print("create person B")
+    print("create person C")
+    print("create person D")
+    print("create person E")
+    print("person A create group with B, C, D")
+    print("person B,C,D accept group invite")
+    print("person A create a meeting entry with group")
+    print("person B,C,D accept meeting invite")
+
+    print("person E create group with D")
+    print("person D accept group invite")
+    print("person E try to create a meeting entry with group that happen to conflict with person A's meeting")
+    print("person E see that person D is not free at that time")
+    print("person E change the meeting entry time")
+    print("person D accept meeting invite")
+
+    print("end demo")
     return
+
 
 if __name__ == '__main__':
     main()
